924ac62549ba3b4fa3b68e6752e61a3e4c01b906
Asen Velkov <114012964+1sen-ski@users.noreply.github.com>
2022-12-09 19:26:54 +0200

    Add files via upload

---
+++ cash_maker.py
@@ -0,0 +1,15 @@
+import time
+import random
+money = 0
+repetitions = 3000
+print("That's the inflation inducing machine, boy.")
+for cash in range(0, repetitions, 1):
+    time.sleep(2)
+    the_easy_money = random.randint(20, 500)
+    print(f"Added cash right now: {the_easy_money}$")
+    money += the_easy_money
+    print(f"Total cash: {money}$")
+    if money >= 400:
+        print(f"Yep, that's enough, now we can buy the guns and proceed with out good deeds!!")
+        print(f"Balance: {money}$")
+        quit()
